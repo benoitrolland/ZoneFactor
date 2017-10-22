4498142cc8faede0b5171cf486f6ec5fa186d7a0
benoitrolland <benoit.rolland@free.fr>
2017-10-22 11:35:50 +0200

    app.module.ts update with installed modeules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+//http://www.learn-angular.fr/le-module-httpmodule/
+import { HttpModule, JsonpModule } from '@angular/http';
+import { FormsModule }      from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule,MatSlideToggleModule,MatSliderModule,MatSelectModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +16,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    ,FormsModule
+    ,HttpModule
+    ,JsonpModule
+    ,FlexLayoutModule
+    ,BrowserAnimationsModule
+    ,MatButtonModule
+    ,MatMenuModule
+    ,MatCardModule
+    ,MatToolbarModule
+    ,MatIconModule
+    ,MatSlideToggleModule
+    ,MatSliderModule
+    ,MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
