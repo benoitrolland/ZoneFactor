e9b72f4f120d510a33bf5bd2efb5fdd702f6482d
benoitrolland <benoit.rolland@free.fr>
2017-10-27 00:52:06 +0200

    librairies et composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map'
-
+import 'rxjs/add/operator/map';
+import 'hammerjs/hammer';
+import { FormControl }      from '@angular/forms';
+	
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Resultats';
+  myControl: FormControl;
   myPicMetaData: Array<any>;
+  worldMapURL: "assets/img/world.svg";
+  continent: Array<any>;
+  country: Array<any>;
+  state: Array<any>;
+  region: Array<any>;  
+  departement: Array<any>;
 
   constructor(private http:Http) {
-    
+    this.myControl = new FormControl();
     this.http.get('https://jsonplaceholder.typicode.com/photos')
       .map(response => response.json())
       .subscribe(res => this.myPicMetaData = res);
+	  
+	//https://stackoverflow.com/questions/36368405/how-to-parse-xml-in-angular-2
+	//xml2js = require('xml2js');
+	//var parser = new xml2js.Parser({explicitArray : false});
+	//used xml2js parser from npm (https://www.npmjs.com/package/xml2js)
+	//and in my service i used this 
+//	autocompleteGeoZones = this.http.get(this.worldMapURL)
+//					.flatMap(res=>{
+//							return Observable.fromPromise(this.getJSON(res.text()))
+//					})
+//					.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+//	autocompleteGeoZones
 
   }
 }
