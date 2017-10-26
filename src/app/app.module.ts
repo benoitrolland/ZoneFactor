f4de4a7b0514669b06a69abde25d3ea1943f9b69
benoitrolland <benoit.rolland@free.fr>
2017-10-27 00:46:26 +0200

    libraries added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 //http://www.learn-angular.fr/le-module-httpmodule/
 import { HttpModule, JsonpModule } from '@angular/http';
-import { FormsModule }      from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule,MatSlideToggleModule,MatSliderModule,MatSelectModule } from '@angular/material';
+import { MatInputModule , MatAutocompleteModule, MatListModule, MatSidenavModule, MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule,MatSlideToggleModule,MatSliderModule,MatSelectModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
     ,FormsModule
+	,ReactiveFormsModule
     ,HttpModule
     ,JsonpModule
     ,FlexLayoutModule
     ,BrowserAnimationsModule
+	,MatInputModule 
+	,MatAutocompleteModule
+	,MatListModule
+    ,MatSidenavModule
     ,MatButtonModule
     ,MatMenuModule
     ,MatCardModule
